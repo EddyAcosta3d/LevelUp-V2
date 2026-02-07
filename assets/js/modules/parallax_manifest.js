8014bcb40c4dd513f00f8fab23ba947427129272
EddyAcosta3d <edacosta3d@gmail.com>
2026-02-07 16:38:43 -0700

    Add files via upload

---
+++ assets/js/modules/parallax_manifest.js
@@ -0,0 +1,47 @@
+window.__PARALLAX_MANIFEST__ = {
+  "alexis": {
+    "fg": "assets/parallax/alexis_fg.webp",
+    "bg": "assets/parallax/alexis_bg.webp",
+    "mid": ""
+  },
+  "arely": {
+    "fg": "assets/parallax/arely_fg.webp",
+    "bg": "assets/parallax/arely_bg.webp",
+    "mid": ""
+  },
+  "brandon": {
+    "fg": "assets/parallax/brandon_fg.webp",
+    "bg": "assets/parallax/brandon_bg.webp",
+    "mid": ""
+  },
+  "eddy": {
+    "fg": "assets/parallax/eddy_fg.webp",
+    "bg": "assets/parallax/eddy_bg.webp",
+    "mid": ""
+  },
+  "ernesto": {
+    "fg": "assets/parallax/ernesto_fg.webp",
+    "bg": "assets/parallax/ernesto_bg.webp",
+    "mid": ""
+  },
+  "maia": {
+    "fg": "assets/parallax/maia_fg.webp",
+    "bg": "assets/parallax/maia_bg.webp",
+    "mid": ""
+  },
+  "majo": {
+    "fg": "assets/parallax/majo_fg.webp",
+    "bg": "assets/parallax/majo_bg.webp",
+    "mid": ""
+  },
+  "tadeo": {
+    "fg": "assets/parallax/tadeo_fg.webp",
+    "bg": "assets/parallax/tadeo_bg.webp",
+    "mid": ""
+  },
+  "thaily": {
+    "fg": "assets/parallax/thaily_fg.webp",
+    "bg": "assets/parallax/thaily_bg.webp",
+    "mid": ""
+  }
+};
